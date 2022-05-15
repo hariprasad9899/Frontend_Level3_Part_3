5cf27f60776aa5ed99bf780d4878b50ea6ef6189
hariprasad9899 <hariprasaadsoundararajan@gmail.com>
2022-05-16 02:19:31 +0530

    all done

--- manage-landing-page-master/app.js
+++ manage-landing-page-master/app.js
@@ -51,3 +51,37 @@ window.addEventListener('click', (e)=> {
         }
     }
 })
+
+const scrollContainer = document.querySelector(".partFive");
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    let w = scrollContainer.clientWidth;
+    scrollContainer.scrollLeft += evt.deltaY;
+});
+
+
+function ValidateEmail(val) {   
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val)){
+        return (true)
+    }
+        return (false)
+}
+
+const go = document.getElementById('go')
+const inp = document.getElementById('inp');
+const err = document.getElementById('err');
+
+go.addEventListener('click', () => {
+    let inpVal = inp.value;
+    let result = ValidateEmail(inpVal);
+    if(result) {
+        inp.style.outline = "3px solid green"
+    } else {
+        err.style.opacity = "1";
+        inp.style.outline = "1px solid red";
+    }
+})
+inp.addEventListener('input', ()=> {
+    inp.style.outline = "none";
+    err.style.opacity = "0";
+})
